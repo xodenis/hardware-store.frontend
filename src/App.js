43eec01cef9e11584254d36be4fd4e370c75bc59
Ruslan Mingaleev <neoch56@mail.ru>
2022-04-13 23:55:03 +0300

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout/Layout'
 
 function App() {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/home" />} />
+        <Route path="home" element={<>Главная</>} />
+        <Route path="contacts" element={<>Контакты</>} />
+        <Route path="delivery" element={<>Доставка</>} />
+        <Route path="favorites" element={<>Избранное</>} />
+        <Route path="profile" element={<>Личный кабинет</>} />
+        <Route path="cart" element={<>Корзина</>} />
+        <Route path="payment-info" element={<>Способы оплаты</>} />
+      </Routes>
+    </Layout>
+  )
 }
 
 export default App
