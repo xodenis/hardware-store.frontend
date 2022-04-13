7699aca63a743dfadc2aed52283769dc7e41b87b
Ruslan Mingaleev <neoch56@mail.ru>
2022-04-13 18:14:09 +0300

    Import fonts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
@@ -13,6 +14,8 @@ const root = createRoot(container)
 
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
 )
