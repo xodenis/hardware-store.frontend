730ff2b372554d9b2eea497c5d311cddb4691ae7
Ruslan Mingaleev <neoch56@mail.ru>
2022-05-17 17:43:06 +0300

    Refactor ProductCard component

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
-import { Col } from 'react-bootstrap'
+import { Col, FormCheck, Row } from 'react-bootstrap'
 import { store } from '../../app/store'
 import { addProduct, removeProduct } from '../../services/cart'
 
@@ -10,7 +10,11 @@ const ProductCard = ({
   product,
   initialCount = 1,
   cartMode = false,
+  orderMode = false,
   className = 'productCard',
+  isSelected = false,
+  onSelect,
+  onUnselect,
 }) => {
   const [count, setCount] = useState(initialCount)
 
@@ -37,22 +41,122 @@ const ProductCard = ({
     store.dispatch(addProduct({ productId, count }))
   }
 
-  const handleClickRemove = (productId) => {
-    store.dispatch(removeProduct(productId))
+  const CartItem = () => {
+    const [selected, setSelected] = useState(isSelected)
+
+    const handleChange = (e) => {
+      if (e.target.checked === true) onSelect(product.id, initialCount)
+      else onUnselect(product.id)
+      setSelected(e.target.checked)
+    }
+
+    const handleClickRemove = (productId) => {
+      store.dispatch(removeProduct(productId))
+      if (selected) onUnselect(productId)
+    }
+
+    return (
+      <Col className={`${className}`}>
+        <div className={`${className}-content`}>
+          <FormCheck
+            type="checkbox"
+            checked={selected}
+            onChange={(e) => handleChange(e)}
+          />
+          <a href={`/product/${product.id}`} className={`${className}-image`}>
+            <img
+              src={'data:image/jpeg;base64,' + product.image}
+              alt={product.name}
+            />
+          </a>
+          <a
+            href={`/product/${product.id}`}
+            className={`${className}-name`}
+            title={product.name}>
+            <span>{product.name}</span>
+          </a>
+          <span className={`${className}-price`}>
+            {product.price + ' ₽/' + product.priceInfo}
+          </span>
+          <div className={`${className}-counter`}>
+            <button
+              className={`${className}-counter-decrement`}
+              onClick={() => handleClickDecrement()}>
+              <FontAwesomeIcon icon="fa-solid fa-minus" />
+            </button>
+            <input
+              type="text"
+              className={`${className}-counter-count`}
+              value={count}
+              onChange={handleCountChange}
+            />
+            <button
+              className={`${className}-counter-increment`}
+              onClick={() => handleClickIncrement()}>
+              <FontAwesomeIcon icon="fa-solid fa-plus" />
+            </button>
+          </div>
+          <span className={`${className}-totalPrice`}>
+            {Math.round(initialCount * product.price * 100) / 100 + ' ₽'}
+          </span>
+          <button
+            className={`${className}-remove`}
+            onClick={() => handleClickRemove(product.id)}>
+            <FontAwesomeIcon icon="fa-regular fa-trash-can" />
+          </button>
+        </div>
+      </Col>
+    )
   }
+
+  const OrderItem = () => {
+    return (
+      <Col className={`${className}`}>
+        <Row className={`${className}-content`}>
+          <Col xs={7} className={`${className}-info`}>
+            <a href={`/product/${product.id}`} className={`${className}-image`}>
+              <img
+                src={'data:image/jpeg;base64,' + product.image}
+                alt={product.name}
+              />
+            </a>
+            <a
+              href={`/product/${product.id}`}
+              className={`${className}-name`}
+              title={product.name}>
+              <span>{product.name}</span>
+            </a>
+          </Col>
+          <Col xs={3} className={`${className}-count`}>
+            <span>{initialCount + ' шт.'}</span>
+          </Col>
+          <Col xs={2}>
+            <span className={`${className}-price`}>
+              {product.price * initialCount + ' ₽'}
+            </span>
+          </Col>
+        </Row>
+      </Col>
+    )
+  }
+
+  if (cartMode) return <CartItem />
+
+  if (orderMode) return <OrderItem />
 
   return (
     <Col className={`${className}`}>
       <div className={`${className}-content`}>
-        {!cartMode && (
-          <button
-            className={`${className}-favorites`}
-            title="Добавить в избранное">
-            <FontAwesomeIcon icon="fa-regular fa-heart" />
-          </button>
-        )}
+        <button
+          className={`${className}-favorites`}
+          title="Добавить в избранное">
+          <FontAwesomeIcon icon="fa-regular fa-heart" />
+        </button>
         <a href={`/product/${product.id}`} className={`${className}-image`}>
-          <img src={'data:image/jpeg;base64,' + product.image} />
+          <img
+            src={'data:image/jpeg;base64,' + product.image}
+            alt={product.name}
+          />
         </a>
         <a
           href={`/product/${product.id}`}
@@ -81,25 +185,11 @@ const ProductCard = ({
             <FontAwesomeIcon icon="fa-solid fa-plus" />
           </button>
         </div>
-        {cartMode && (
-          <span className={`${className}-totalPrice`}>
-            {Math.round(initialCount * product.price * 100) / 100 + ' руб.'}
-          </span>
-        )}
-        {cartMode && (
-          <button
-            className={`${className}-remove`}
-            onClick={() => handleClickRemove(product.id)}>
-            <FontAwesomeIcon icon="fa-regular fa-trash-can" />
-          </button>
-        )}
-        {!cartMode && (
-          <button
-            className={`${className}-addToCart`}
-            onClick={() => addToCart(product.id, count)}>
-            В корзину
-          </button>
-        )}
+        <button
+          className={`${className}-addToCart`}
+          onClick={() => addToCart(product.id, count)}>
+          В корзину
+        </button>
       </div>
     </Col>
   )
